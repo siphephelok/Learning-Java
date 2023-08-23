976aaa1c02e4dc848c8aa9a7fd7df6f81b424702
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-23 22:42:26 +0200

    Added Comments

--- First.java
+++ First.java
@@ -1,5 +1,9 @@
 public class First {
     public static void main(String[] args){
+        // This is a single line commment
+        /*And this is a multiline comment
+        * as you can see
+        * yourself*/
         System.out.println("My FIRST JAVA PROGRAM");
     }
 }
