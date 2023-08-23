da15ab65aaa5ba6ed94f2472c069fc0dd7c3c91a
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-23 22:54:43 +0200

    Variables

--- First.java
+++ First.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class First {
     public static void main(String[] args){
         // This is a single line commment
@@ -5,5 +7,13 @@ public class First {
         * as you can see
         * yourself*/
         System.out.println("My FIRST JAVA PROGRAM");
+
+        //Creating Variables
+
+        int number = 10;
+        String name = "Siphephelo Myeni";
+        Date date = new Date();
+        System.out.println("My name is: " + name + " , my fav number is " + number);
+        System.out.println("Today's date is: " + date);
     }
 }
